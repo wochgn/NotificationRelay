8a6e3e3b8d30a82630ecf3bbb75a308f18bf804f
InvalidName <wochgn@gmail.com>
2026-08-27 23:07:02 +0800

    chore: 版本号提升至 1.3 (versionCode 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.notifrelay"
         minSdk = 31
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
     }
 
     buildTypes {
